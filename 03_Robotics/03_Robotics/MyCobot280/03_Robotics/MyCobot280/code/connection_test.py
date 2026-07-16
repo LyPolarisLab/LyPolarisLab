7cf5783f2524763e0e6595a963fdb72339354813
LyPolarisLab <lyvelvet031@gmail.com>
2026-07-16 19:16:49 +0200

    Add connection test script for MyCobot 280
    This script connects to the MyCobot 280 robot, checks communication, retrieves firmware version, and reads joint angles.

---
+++ 03_Robotics/03_Robotics/MyCobot280/03_Robotics/MyCobot280/code/connection_test.py
@@ -0,0 +1,29 @@
+"""
+MyCobot 280 - Connection Test
+
+First experiment:
+- Connect Python with the robot
+- Check robot communication
+- Read robot information
+
+Author: LyPolarisLab
+"""
+
+
+
+from pymycobot import MyCobot280
+import time 
+
+#Connexion au robot sur le port COM3
+mc = MyCobot280('COM3', 115200)
+time.sleep(2) # Attente de 2 secondes pour que le robot se connecte correctement
+
+#Test : demander la version du firmware 
+print("Version : ", mc.get_system_version())
+
+#Test : Lire les angles actuels des 6 joints du robot
+print("Angles actuels : ", mc.get_angles())
+
+#Configuration des angles de calibration pour chaque joint du robot
+mc.send_angles([0, 0, 0, 0, 0, 0], 30)
+
